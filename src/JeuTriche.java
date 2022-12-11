d95e56494967dd77dca36445a79dc7b8beda9802
MrMA2B <92058008+MrMA2B@users.noreply.github.com>
2022-12-11 20:13:19 +0100

    Add comments

--- src/JeuTriche.java
+++ src/JeuTriche.java
@@ -1,3 +1,10 @@
+
+/**
+ * This class define the gameplay. Ie. all the proccess to play
+ *
+ * @author Adrien MATTEI, Thierry RINGLET, Felix HUMEAU, Augustin MEAUDRE
+ */
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +28,8 @@ public class JeuTriche implements Serializable {
 
 	public void newGame() {
 
+		// On crée tous les objets pour le robots et le joueur
+
 		System.out.println("\nVous jouez contre un ordinateur. Saurez-vous vous montrer plus stratégique que lui ?");
 
 		Joueur joueur = new Joueur();
@@ -43,10 +52,12 @@ public class JeuTriche implements Serializable {
 				Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
 		Random random = new Random();
 
+		// Ici on test pour chaque tour si il reste des navires à toucher
 		while ((generalPvH > 0) && (generalPvR > 0)) {
 
 			System.out.println("\nC'est votre tour moussaillon ! \n");
 
+			// Rq on affiche notre Grille et celle du Robot car on est dans le mode Triche
 			System.out.println("Voici votre grille Amiral : ");
 			grilleH.afficher();
 			System.out.println("Voici la grille ennemie : ");
@@ -57,6 +68,7 @@ public class JeuTriche implements Serializable {
 
 			int actionChoice = 0;
 
+			// On blinde les entrées utilisateurs
 			while ((actionChoice != 1) && (actionChoice != 2)) {
 				try {
 					System.out.print("\nEn attente d'ordre d'action : ");
@@ -70,11 +82,12 @@ public class JeuTriche implements Serializable {
 				}
 			}
 
+			// Si 1 on tire
 			if (actionChoice == 1) {
-
 				System.out.print("\nDepuis quel navire voulez-vous tirer ?");
 				String targetChoice = sc1.nextLine();
 
+				// On blinde les entrées utilisateurs
 				while (!listOfBoatRadomTargetChoice.contains(targetChoice)) {
 					System.out.println("\nContrordre Amiral ! On attend de vous d'identifier un navire parmi :\n");
 					System.out.println("	S1, D1, F1, et C0");
@@ -82,12 +95,18 @@ public class JeuTriche implements Serializable {
 					targetChoice = sc1.nextLine();
 				}
 
+				// On récupère l'objet navire selectionné par l'utilisateur en entrant un string
+				// correspondant au nom du navire
 				Navire navireH = hashMapBoatsOfJoueur.get(targetChoice);
 
+				// On vérifie que le navire n'a pas déjà été coulé car sinon on ne peut pas
+				// tirer avec et alors on redemande à l'utilisateur d'entrer un autre navire
 				while (navireH.getPv() == 0) {
 					System.out.println("\nVous ne pouvez pas tirer à partir d'un navire coulé");
 					System.out.print("Depuis quel navire voulez-vous tirer ?");
 					targetChoice = sc1.nextLine();
+
+					// On blinde la nouvelle entrée dans le cas où la première ne passe pas
 					while (!listOfBoatRadomTargetChoice.contains(targetChoice)) {
 						System.out
 								.println("\nContrordre Amiral ! On attend de vous d'identifier parmi ceux restant :\n");
@@ -99,11 +118,13 @@ public class JeuTriche implements Serializable {
 
 				boolean testH = false;
 
+				// Tant que le tire n'a pas fonctionné
 				while (testH == false) {
 
 					System.out.print("\nEntrez les coordonées X de votre cible (ex : A, G, O) : ");
 					String targetChar = sc2.nextLine();
 
+					// On blinde l'entrée utilisateur
 					while (!listOfCoordX.contains(targetChar)) {
 						System.out.println(
 								"\nContrordre Amiral ! On attend de vous d'identifier une ligne comprise entre A et O :\n");
@@ -115,6 +136,7 @@ public class JeuTriche implements Serializable {
 
 					int targetY = 22;
 
+					// On blinde l'entrée utilisateur
 					while (!listOfCoordY.contains(targetY)) {
 						try {
 							System.out.print("Entrez les coordonées Y de votre cible (ex : 1, 6, 15) : ");
@@ -127,6 +149,7 @@ public class JeuTriche implements Serializable {
 						}
 					}
 
+					// On tire sur la case selectionne avec le navire souhaité
 					testH = navireH.tirer(targetX, targetY, grilleR, hashMapBoatsOfRobot);
 
 					generalPvH = joueur.getGeneralPvHTriche();
@@ -134,11 +157,14 @@ public class JeuTriche implements Serializable {
 
 				}
 			}
+
+			// Si 2 alors on déplace le navire
 			if (actionChoice == 2) {
 
 				System.out.print("Quel navire voulez-vous déplacer ?");
 				String choosenBoat = sc4.nextLine();
 
+				// On blinde l'entrée utilisateur
 				while (!listOfBoatRadomTargetChoice.contains(choosenBoat)) {
 					System.out.println("\nContrordre Amiral ! On attend de vous d'identifier un navire parmi :\n");
 					System.out.println("	S1, D1, F1, et C0");
@@ -146,10 +172,13 @@ public class JeuTriche implements Serializable {
 					choosenBoat = sc4.nextLine();
 				}
 
+				// On teste si le navire n'a pas déjà été touché car si tel était le cas alors
+				// on ne pourrait plus le déplacer
 				while (hashMapBoatsOfJoueur.get(choosenBoat).getPv() != hashMapBoatsOfJoueur.get(choosenBoat).length) {
 					System.out.println("\nLe bateau selectionné ne peut pas être déplacé car il a déjà été touché.");
 					System.out.print("Quel navire voulez-vous déplacer : ");
 					choosenBoat = sc4.nextLine();
+					// On blinde l'entrée utilisateur
 					while (!listOfBoatRadomTargetChoice.contains(choosenBoat)) {
 						System.out.println(
 								"\nContrordre Amiral ! On attend de vous d'identifier un navire parmi ceux restant :\n");
@@ -158,6 +187,7 @@ public class JeuTriche implements Serializable {
 					}
 				}
 
+				// En fonction de l'axe du navire on propose de déplacer à G/D ou en H/B
 				if (grilleH.getAxe(choosenBoat) == 0) {
 
 					System.out.println("Pour déplacer vers le haut, entrez : H");
@@ -219,12 +249,17 @@ public class JeuTriche implements Serializable {
 							choosenBoat);
 				}
 			}
-			
-			if((generalPvH == 0) || (generalPvR == 0)) {
+
+			// Ici on test si la dernière action de l'Humain n'a pas conduit à l'élimination
+			// du dernier navire au quel cas la partie est terminée et on finit la manche
+			if ((generalPvH == 0) || (generalPvR == 0)) {
 				break;
 			}
 
 			System.out.println("\n Au tour de l'ennemi... \n");
+
+			// Ici c'est exactement le même processus que précédement mais avec des entrées
+			// aléatoires afin de générer le jeu du robot
 
 			int randomActionChoice = random.nextInt(2);
 			int indexOfBoatRadomTargetChoice = random.nextInt(listOfBoatRadomTargetChoice.size());
@@ -233,7 +268,7 @@ public class JeuTriche implements Serializable {
 
 			int randomIndexDirection = random.nextInt(listOfRandomPositionChoice.size());
 			char randomPositionChoice = listOfRandomPositionChoice.get(randomIndexDirection);
-			
+
 			System.out.println("Voici votre grille mon Robot Amiral : ");
 			grilleR.afficher();
 			System.out.println("Voici la grille de l'ennemi humain : ");
@@ -244,9 +279,8 @@ public class JeuTriche implements Serializable {
 			case 0: // Shoot
 
 				int randomTargetX = random.nextInt(15);
-				System.out.println("L'ordinateur à choisi la coordonnée X : " + randomTargetX); // A CORRIGER POUR
-																								// AFFICHER DES "A" "B"
-																								// ....
+				System.out.println("L'ordinateur à choisi la coordonnée X : " + randomTargetX);
+
 				int randomTargetY = random.nextInt(15);
 				System.out.println("L'ordinateur à choisi la coordonnée Y : " + randomTargetY);
 
@@ -329,12 +363,16 @@ public class JeuTriche implements Serializable {
 			generalPvH = joueur.getGeneralPvHTriche();
 			generalPvR = robot.getGeneralPvRTriche();
 		}
-		
+
+		// Ici on sort de la boucle while càd que une des 2 grille a tous ses bateaux
+		// éliminés et alors la partie est terminée
 		System.out.println("La partie est terminée! ");
-		if(generalPvH == 0) {
+		// Ici si les pv de la grille homme sont à 0 alors on affiche que l'homme a
+		// perdu
+		if (generalPvH == 0) {
 			System.out.println("Dommage vous avez perdu.");
-		}
-		else {
+			// Sinon alors on a gagné
+		} else {
 			System.out.println("Bravo vous avez été plus fort que le robot Amiral !");
 		}
 	}
